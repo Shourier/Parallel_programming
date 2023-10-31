bd2b8a4b517811d5da0f0a8f62d38fa80d196183
Sergey <116805000+Shourier@users.noreply.github.com>
2023-11-01 00:25:44 +0300

    Add files via upload

--- Parallel_programming_/Linux_parallel_programming/lab1_func.h
+++ Parallel_programming_/Linux_parallel_programming/lab1_func.h
@@ -1,23 +1,18 @@
-#pragma once
-#ifndef LAB1_FUNC
-#define LAB1_FUNC
+#pragma once 
+#ifndef LAB1_FUNC 
+#define LAB1_FUNC 
 
-#define CHAR_SIZE sizeof(char)
+#define CHAR_SIZE sizeof(char) 
 
-#include <iostream>
-#include <fstream>
+#include <iostream> 
+#include <fstream> 
 
 int binary_array_size = 0;
 int jpg_file_size = 0;
 
 int* open_jpg_file_as_binary_array()
 {
-	FILE* jpg_file = NULL;
-	if (fopen_s(&jpg_file, "1623781112_img0.jpg", "r") != 0)
-	{
-		return NULL;
-		throw std::exception("Cannot open file or it doesn't exist!");
-	}
+	FILE* jpg_file = fopen("1623781112_img0.jpg", "r");
 
 	fseek(jpg_file, 0, SEEK_END);
 	jpg_file_size = ftell(jpg_file);
@@ -42,11 +37,7 @@ int* open_jpg_file_as_binary_array()
 
 void write_data_in_new_file(int* array, const char* file_name)
 {
-	FILE* data_file = NULL;
-	if (fopen_s(&data_file, file_name, "w+") != 0)
-	{
-		throw std::exception("Cannot open file or it doesn't exist!");
-	}
+	FILE* data_file = fopen(file_name, "w+");
 
 	for (int i = 0; i < binary_array_size; i++)
 	{
@@ -66,4 +57,4 @@ int* transforming_function(int* transformed_array)
 	return (result);
 }
 
-#endif 
+#endif
--- Parallel_programming_/Linux_parallel_programming/main.cpp
+++ Parallel_programming_/Linux_parallel_programming/main.cpp
@@ -1,10 +1,13 @@
-#include "lab1_func.h"
-#include <iostream>
+#include "lab1_func.h" 
+#include <iostream> 
+#include <chrono> 
 
-#include <vector>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <chrono>
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/wait.h> 
+#include <cstring> 
+#include <unistd.h> 
 
 using namespace std;
 using namespace chrono;
@@ -13,81 +16,88 @@ const int group_number = 514;
 const int my_number = 17;
 
 const int X = group_number + my_number;
-const int A = X % 4;						// A = 3
-const int B = 7 + X % 7;					// B = 13
+const int A = X % 4;      // A = 3 
+const int B = 7 + X % 7;     // B = 13 
 
 
-#pragma region fork
+#pragma region fork 
 
 void processing_by_fork(int thread_number, int* array, int array_size)
 {
-	for (int i = 0; i < thread_number; i++)
-	{
-		pid_t child_process = fork();
+    for (int i = 0; i < thread_number; i++)
+    {
+        pid_t child_process = fork();
 
-		if (child_process == 0)
-		{
-			for (int j = array_size - i - 1; j >= 0; j -= thread_number)
-			{
-				array[j] = (j * array[j]) & 255;
-			}
-		}
-		else
-		{
-			perror("error while calling fork");
-			exit(1);
-		}
+        if (child_process == 0)
+        {
+            for (int j = array_size - i - 1; j >= 0; j -= thread_number)
+            {
+                array[j] = (j * array[j]) & 255;
+            }
+            exit(0);
+        }
+        else if (child_process < 0)
+        {
+            perror("error while calling fork");
+            exit(1);
+        }
+        else
+        {
+            int status;
+            waitpid(child_process, &status, 0);
+        }
 
-	}
+    }
 
-	for (int i = 0; i < thread_number; i++)
-	{
-		wait(NULL);
-	}
+    for (int i = 0; i < thread_number; i++)
+    {
+        wait(NULL);
+    }
 }
 
-#pragma endregion
+#pragma endregion 
 
 void apply_method(void method(int, int*, int), int thread_number, int* array, int array_size, string method_name, const char* txt_name)
 {
-	auto start = high_resolution_clock::now();
-	method(thread_number, array, array_size);
-	write_data_in_new_file(array, txt_name);
-	auto finish = high_resolution_clock::now();
-	duration<float> proc_time = finish - start;
-	cout << method_name << " method time: " << proc_time.count() << endl;
+    auto start = high_resolution_clock::now();
+    method(thread_number, array, array_size);
+    write_data_in_new_file(array, txt_name);
+    auto finish = high_resolution_clock::now();
+    duration<float> proc_time = finish - start;
+    cout << method_name << " method time: " << proc_time.count() << endl;
 }
 
 int main()
 {
-	try
-	{
-		cout << "A = " << A << "; B = " << B << endl;
+    try
+    {
+        cout << "A = " << A << "; B = " << B << endl;
 
-		int* array = open_jpg_file_as_binary_array();
-		vector<int*> array_copies;
-		for (int i = 0; i < 8; i++)
-		{
-			int* array_copy = new int[binary_array_size];
-			for (int j = 0; j < binary_array_size; j++)
-			{
-				array_copy[j] = array[j];
-			}
-			array_copies.push_back(array_copy);
-		}
+        int* array = open_jpg_file_as_binary_array();
 
-		auto start = high_resolution_clock::now();
-		int* array_1 = transforming_function(array);
-		write_data_in_new_file(array_1, "data.txt");
-		auto finish = high_resolution_clock::now();
-		duration<float> proc_time = finish - start;
-		cout << "Without parallel programming methods time: " << proc_time.count() << endl;
+        auto start = high_resolution_clock::now();
+        int* array_1 = transforming_function(array);
+        write_data_in_new_file(array_1, "data.txt");
+        auto finish = high_resolution_clock::now();
+        duration<float> proc_time = finish - start;
+        cout << "Without parallel programming methods time: " << proc_time.count() << endl;
 
-		
-		apply_method(processing_by_fork, B, array_copies[4], binary_array_size, "processing_by_CreateProcess", "processing_by_CreateProcess.txt");
-	}
-	catch (const exception& e)
-	{
-		cout << e.what();
-	}
+        key_t key = ftok(".", 'S');
+        int shmid = shmget(key, binary_array_size * sizeof(int), IPC_CREAT | 0666);
+
+        int* array_copy_0 = (int*)shmat(shmid, NULL, 0);
+        for (int j = 0; j < binary_array_size; j++)
+        {
+            array_copy_0[j] = array[j];
+        }
+        apply_method(processing_by_fork, B, array_copy_0, binary_array_size, "processing_by_fork", "processing_by_fork.txt");
+        for (int i = 0; i < 100; i++)
+        {
+            cout << array_copy_0[i] << " ";
+        }
+    }
+    catch (const exception& e)
+    {
+        cout << e.what();
+    }
 }
